360897b21d88a4f662ecc5de6ae27db0b102ebcb
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-11-03 23:17:06 -0300

    Created controllers

---
+++ backend/src/controllers/UserController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+
+const users = [
+   { name: 'Diego', email: 'diego@rocketseat.com.br' },
+];
+
+export default {
+   async index(req: Request, res: Response) {
+      return res.json(users);
+   }
+};
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
+import UserController from './controllers/UserController';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-   return res.send('Hello World');
-})
+
+routes.get('/users', UserController.index);
 
 
 export default routes;
