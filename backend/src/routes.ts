f6730f7c92e8e39e189dd887f6a366b7deedef2a
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-11-05 22:58:25 -0300

    Changed route get to post

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,7 +5,7 @@ const routes = Router();
 
 
 routes.get('/users', UserController.index);
-routes.get('/users/create', UserController.create);
+routes.post('/users', UserController.create);
 
 
 export default routes;
