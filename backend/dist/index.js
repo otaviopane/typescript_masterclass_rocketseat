8231a809fa613ff79200f7529d7d3cf95bc3dcf6
Otavio Ferraroni G Pane <otavio.pane@gmail.com>
2021-11-05 23:50:28 -0300

    Prepared the frontend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,9 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    return res.send('Hello World');
-});
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var routes_1 = __importDefault(require("./routes"));
+var app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(routes_1.default);
 app.listen(3333);
